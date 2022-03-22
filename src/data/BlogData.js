00af9da16f6e1a7582e47b5f209eb5dce93fe1b3
André Hatlo-Johansen <ahatloj@gmail.com>
2022-03-22 09:14:20 +0100

    Init Commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,69 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"",
+        tags:["","",""],
+        date:"1 Januar, 2022",
+        imgSrc:"",
+        link:""
+    },
+    {
+        id:2,
+        name:"",
+        tags:["",""],
+        date:"1 February, 2022",
+        imgSrc:"",
+        link:""
+    },
+    {
+        id:3,
+        name:"",
+        tags:["","",""],
+        date:"1 March, 2022",
+        imgSrc:"",
+        link:""
+    },
+    {
+        id:4,
+        name:"",
+        tags:["","",""],
+        date:"1 April, 2022",
+        imgSrc:"",
+        link:""
+    },
+    {
+        id:5,
+        name:"",
+        tags:["","",""],
+        date:"1 June, 2022",
+        imgSrc:"",
+        link:""
+    },
+    {
+        id:6,
+        name:"",
+        tags:["",""],
+        date:"1 July, 2022",
+        imgSrc:"",
+        link:""
+
+    },
+   
+    {
+        id:7,
+        name:"",
+        tags:["","",""],
+        date:"1 August, 2022",
+        imgSrc:"h",
+        link:""
+    },
+    {
+        id:8,
+        name:"",
+        tags:["","",""],
+        date:"1 September, 2022",
+        imgSrc:"",
+        link:""
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,61 @@
+export const Work = [
+    {
+        id:1,
+        name:"Landing Page",
+        description:"",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"",
+        github:"https://github.com/andrehatlo/"
+    },
+    {
+        id:2,
+        name:"Website",
+        description:".",
+        tags:["react","sass"],
+        
+        demo:"",
+        github:"https://github.com/andrehatlo/"
+        
+    },
+    {
+        id:3,
+        name:"Test",
+        description:"",
+        tags:["react","api","sass"],
+        
+        demo:"",
+        github:"https://github.com/andrehatlo/"
+    },
+    {
+        id:4,
+        name:"Sidebar Menu",
+        description:"",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"",
+        github:"https://github.com/andrehatlo/"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"",
+        github:"https://github.com/andrehatlo/"
+        
+    },
+    {
+        id:6,
+        name:"Responsive Navigation Menu",
+        description:"",
+        tags:["react","react-router","css"],
+        
+        demo:"",
+        github:"https://github.com/andrehatlo/"
+        
+    },
+    
+  
+] 
